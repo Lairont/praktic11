c71629828e613c32a7aecc154e55ec6282717c44
Никита <153456515+Lairont@users.noreply.github.com>
2024-12-10 11:20:39 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /// <summary>
+        /// Задача 1. Определить количество элементов одномерного массива.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int[] omas = { 1, 3, 5, 6, 2 };
+            Console.WriteLine($"Размер: {omas.Length}") ;
+            string[] mas = { "Антон","Данил","Никита","Артем" };
+            int size = mas.Length;
+            Console.WriteLine($"Размер: {size}");
+            Console.ReadLine();
+        }
+    }
+}
