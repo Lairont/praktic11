f680b1bc476fc7433db2d442881e3646d5388187
Никита <153456515+Lairont@users.noreply.github.com>
2024-12-10 13:21:51 +0200

    Update Program.cs

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -18,7 +18,6 @@ namespace ConsoleApp5
             int[] array = { 1, 2, 3, 4, 2, 2, 5, 6, 2 };
             int tar = 2;
             int count = array.Count(i => i == tar);
-            int cont = array.Count(i => i == tar);
             Console.WriteLine($"Число {tar} встречается {count} раз(а) в массиве.");
             Console.ReadLine();
         }
