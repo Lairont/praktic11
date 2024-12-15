6abfdf5f22f1b18a919dcc11bc3b286f513fa98e
Никита <153456515+Lairont@users.noreply.github.com>
2024-12-16 00:11:15 +0200

    Add files via upload

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    /// <summary>
+    /// Задача 11. Сумма всех элементов массива.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = { 1, 3, 5, 7, 4, 23, 4, 5, 7 };
+            int sum = omas.Sum();
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
