c0a7d428c55faf38429ed23490061f7812a20a1c
Никита <153456515+Lairont@users.noreply.github.com>
2024-12-16 00:21:10 +0200

    Update Program.cs

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp10
+namespace ConsoleApp11
 {
     /// <summary>
     /// Задача 11. Сумма всех элементов массива.
