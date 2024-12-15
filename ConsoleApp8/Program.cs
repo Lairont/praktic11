42e5d81652d3b41ce34ed6e3b232358273201c08
Никита <153456515+Lairont@users.noreply.github.com>
2024-12-16 00:15:34 +0200

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    /// <summary>
+    /// Задача 8. Изменение размера массива.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 4, 7, 56, 9, 3, 6, 0 };
+            Array.Resize(ref array, 4);
+            foreach (var element in array)
+            {
+                Console.WriteLine(element);
+            }
+            Console.ReadLine();
+        }
+    }
+}
