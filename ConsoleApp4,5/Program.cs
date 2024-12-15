1844910e76ef06785a619f8b1eaaddbbe990f329
Никита <153456515+Lairont@users.noreply.github.com>
2024-12-16 00:03:03 +0200

    Add files via upload

---
+++ ConsoleApp4,5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    /// <summary>
+    /// Задача 4. Сортировка массива по возрастанию.
+    /// Задача 5. Реверс массива.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = { "Tom", "Sam", "Bob", "Kate", "Tom", "Alice" };
+            Array.Sort(people);
+            foreach (var element in people)
+            {
+                Console.WriteLine(element);
+            }
+            Array.Reverse(people);
+            foreach (var element in people)
+            {
+                Console.WriteLine(element);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
