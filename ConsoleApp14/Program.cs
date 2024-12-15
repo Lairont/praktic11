172b6b3cc8f5fe5a5f436b5025d229295ff39ba3
Никита <153456515+Lairont@users.noreply.github.com>
2024-12-16 00:17:19 +0200

    Update Program.cs

--- ConsoleApp14/Program.cs
+++ ConsoleApp14/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp4
+namespace ConsoleApp14
 {
     /// <summary>
     /// Задача 14. Дан курс рубля России за январь 2023 года. Рассчитать:
