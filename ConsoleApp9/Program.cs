1e3b76e6135c55239280b1e8c1ef79bf3d97389c
Никита <153456515+Lairont@users.noreply.github.com>
2024-12-15 23:53:58 +0200

    Add files via upload

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 56, 7, 3, 7, 12, 67, 4, 5, 2, 6, 9, };
+            int index = Array.IndexOf(array, 12);
+            Console.WriteLine(index);     
+            Console.ReadLine();
+        }
+    }
+}
