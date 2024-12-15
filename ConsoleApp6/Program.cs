be5283c7ec03f2284e7dd4f3b835ca8b4c513932
Никита <153456515+Lairont@users.noreply.github.com>
2024-12-16 00:06:11 +0200

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    /// <summary>
+    /// Задача 6. Подсчет количества вхождений определенного числа в массив.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = { 1, 3, 5, 7, 5, 2, 3, 4, 5, 6 };
+            int count = omas.Count(x => x == 5);
+            Console.WriteLine(count);
+            Console.ReadLine();
+
+        }
+    }
+}
